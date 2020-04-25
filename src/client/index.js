c1db4ca8247f82db2b66fabf67cf2a916ac3d504
Takudzwa Chidarikire <briansaved@gmail.com>
2020-04-26 00:34:14 +1000

    tests

--- src/client/index.js
+++ src/client/index.js
@@ -1,9 +1,13 @@
-import { checkForName } from './js/nameChecker'
-import { handleSubmit } from './js/formHandler'
+import { checkForName } from "./js/nameChecker";
+import { handleSubmit } from "./js/formHandler";
 
-import './styles/resets.scss'
-
+/*import "./src/client/styles/resets.scss";
+import "./src/client/styles/base.scss";
+import "./src/client/styles/footer.scss";
+import "./src/client/styles/form.scss";
+import "./src/client/styles/header.scss";
+*/
 console.log(checkForName);
 
-alert("I EXIST")
+alert("I EXIST");
 console.log("CHANGE!!");
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,23 +1,29 @@
-const path = require('path')
-const webpack = require('webpack')
-const HtmlWebPackPlugin = require("html-webpack-plugin")
+const path = require("path");
+const webpack = require("webpack");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    entry: './src/client/index.js',
-    mode: 'production',
-    module: {
-        rules: [
-            {
-                test: '/\.js$/',
-                exclude: /node_modules/,
-                loader: "babel-loader"
-            }
-        ]
-    },
-    plugins: [
-        new HtmlWebPackPlugin({
-            template: "./src/client/views/index.html",
-            filename: "./index.html",
-        })
-    ]
-}
+  entry: "./src/client/index.js",
+  output: {
+    path: path.join(__dirname, "dist"),
+    filename: "bundle.min.js",
+    libraryTarget: "var",
+    library: "Client",
+  },
+  mode: "production",
+  module: {
+    rules: [
+      {
+        test: "/.js$/",
+        exclude: /node_modules/,
+        loader: "babel-loader",
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./src/client/views/index.html",
+      filename: "./index.html",
+    }),
+  ],
+};
