57f39d12588ba72d9a5d6504de7564c8be453a05
Takudzwa Chidarikire <briansaved@gmail.com>
2020-04-26 14:33:34 +1000

    JS Fixed

--- src/client/index.js
+++ src/client/index.js
@@ -7,6 +7,8 @@ import "./styles/footer.scss";
 import "./styles/form.scss";
 import "./styles/header.scss";
 
+export { checkForName, handleSubmit };
+
 console.log(checkForName);
 
 alert("I EXIST");
