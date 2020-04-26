1ef4bf15a75744d5239d870e243449c406217bb6
Takudzwa Chidarikire <briansaved@gmail.com>
2020-04-26 21:13:00 +1000

    Records Temperature on Page

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -2,14 +2,16 @@ function handleSubmit(event) {
   event.preventDefault();
 
   // check what text was put into the form field
+  const apiKey = `&APPID=4f2ae032089f5dece0abe5982bc51612`;
+  const baseUrl = `https://api.openweathermap.org/data/2.5/weather?q=`;
   let formText = document.getElementById("name").value;
-  Client.checkForName(formText);
+  //Client.checkForName(formText);
 
   console.log("::: Form Submitted :::");
-  fetch("/test")
+  fetch(baseUrl + formText + apiKey)
     .then((res) => res.json())
     .then(function (res) {
-      document.getElementById("results").innerHTML = res.message;
+      document.getElementById("results").innerHTML = res.main.temp;
     });
 }
 
